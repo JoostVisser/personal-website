3d76f31aad6bfdb8bb9933b82dca9a94e29641cc
Joost Visser <jwjhvisser@gmail.com>
2024-05-24 12:24:22 +0200

    fix test, change playwright to container

--- tests/test.ts
+++ tests/test.ts
@@ -2,5 +2,5 @@ import { expect, test } from '@playwright/test';
 
 test('home page has expected h1', async ({ page }) => {
 	await page.goto('/');
-	await expect(page.getByRole('heading', { name: "Hi, I'm Joost" })).toBeVisible();
+	await expect(page.getByRole('heading', { name: "Hey, I'm Joost" })).toBeVisible();
 });
