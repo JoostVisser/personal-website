157e189ae89e9128e5292cfbe33fe2ca92144092
Joost Visser <jwjhvisser@gmail.com>
2025-06-07 18:09:45 +0200

    Fix tests

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,15 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { enhancedImages } from '@sveltejs/enhanced-img';
-import { UserConfig } from 'vite';
+import type { UserConfig } from 'vite';
 import { svelteTesting } from '@testing-library/svelte/vite';
 import tailwindcss from '@tailwindcss/vite';
 
 export default {
 	plugins: [enhancedImages(), tailwindcss(), sveltekit(), svelteTesting()],
-	environment: {
+	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}'],
-		environment: 'jsdom'
+		environment: 'jsdom',
+		globals: true,
+		setupFiles: ['tests/setup/setup.ts']
 	}
 } satisfies UserConfig;
